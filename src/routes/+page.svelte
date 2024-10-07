8f6012213b56bbb452cef765afe7f2c2fe24eaea
khooi <khooi.dev@gmail.com>
2024-10-07 16:31:07 +0700

    added production notice

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,37 @@
-<div>
+<section>
   <img
     src="/tiles-logo.png"
     alt="Logo of NUNG Exhibition, featuring 4 aesthetic tiles patterns representing 4 disciplines of design found within the exhibition."
   />
-  <h2>
-    “Nung” is a Capstone 2 Exhibition, showcasing the works of RMIT University’ Final Year Design
-    Students. The concept of the project revolves around the imagery of different types of brick
-    tile and the process of making bricks. The clay has to be molded, transformed, and fired (nung)
-    under great heat in order to become a finished brick, which is a metaphor for design students
-    undergoing many hardships before submitting their final works, showing themselves to the world.
-  </h2>
-</div>
+  <div>
+    <h2>
+      “Nung” is a Capstone 2 Exhibition, showcasing the works of RMIT University’ Final Year Design
+      Students. The concept of the project revolves around the imagery of different types of brick
+      tile and the process of making bricks. The clay has to be molded, transformed, and fired
+      (nung) under great heat in order to become a finished brick, which is a metaphor for design
+      students undergoing many hardships before submitting their final works, showing themselves to
+      the world.
+    </h2>
+    <p>
+      The website is currently in production. Still, feel free to roam about the website and see it
+      grow each day. Be aware of bugs and irregularities in this stage.
+    </p>
+  </div>
+</section>
 
 <style>
-  div {
+  section {
     background-color: #c72027;
     height: 100vh;
     display: flex;
     justify-content: space-between;
     align-items: start;
+  }
+
+  div {
+    width: 750px;
+    margin: 120px 30px 0 30px;
+    padding: 0;
   }
 
   img {
@@ -28,9 +41,15 @@
   }
 
   h2 {
-    width: 750px;
     color: white;
     font-size: 24px;
-    margin: 120px 30px 0 30px;
+    margin: 0;
+  }
+
+  p {
+    color: white;
+    font-size: 24px;
+    font-style: italic;
+    font-weight: 200;
   }
 </style>
