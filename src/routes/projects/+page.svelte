0355555d0f22e753276108d4fd01a6e60c72117b
khooi-lpt <khooi.tr@gmail.com>
2024-10-12 18:08:34 +0700

    Projects List logic update

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,26 +1,27 @@
+<script>
+  let projects = [
+    {
+      file: 'bach-khoa-khong-toan-thu',
+      name: 'Bách Khoa Không Toàn Thư',
+      author: 'Trần Khánh Linh',
+    },
+    {
+      file: 'algorithmic-beauty',
+      name: 'Algorithmic Beauty',
+      author: 'Từ Bảo Ngân',
+    },
+  ];
+</script>
+
 <ul>
-  <li><a href="/projects/bach-khoa-khong-toan-thu">Bách Khoa Không Toàn Thư</a></li>
-  <li><a href="/projects/bach-khoa-khong-toan-thu">Culture Archive-Ethnicities</a></li>
-  <li>
-    <a href="/projects/bach-khoa-khong-toan-thu">Yêu Dấu - A Guide for Learning Vietnamese Tones</a>
-  </li>
-  <li><a href="/projects/bach-khoa-khong-toan-thu">Adopt to Adore</a></li>
-  <li><a href="/projects/bach-khoa-khong-toan-thu">Expose</a></li>
-  <li>
-    <a href="/projects/bach-khoa-khong-toan-thu">Changes - Perspectives on an Evolving City</a>
-  </li>
-  <li><a href="/projects/bach-khoa-khong-toan-thu">Entwine Garden</a></li>
-  <li><a href="/projects/bach-khoa-khong-toan-thu">Green Journey</a></li>
-  <li><a href="/projects/bach-khoa-khong-toan-thu">Timewise</a></li>
-  <li><a href="/projects/bach-khoa-khong-toan-thu">Mark of Identity</a></li>
-  <li>
-    <a href="/projects/bach-khoa-khong-toan-thu">Sinh Học Thần Thú (Biology of the Holy Beast)</a>
-  </li>
-  <li><a href="/projects/bach-khoa-khong-toan-thu">Diệu Phục</a></li>
-  <li><a href="/projects/bach-khoa-khong-toan-thu">The Flow</a></li>
-  <li><a href="/projects/bach-khoa-khong-toan-thu">Algorithmic Beauty</a></li>
-  <li><a href="/projects/bach-khoa-khong-toan-thu">Ngọc Linh - Roots to the Past</a></li>
-  <li><a href="/projects/bach-khoa-khong-toan-thu">Thắt Exhibition (Tighten)</a></li>
+  {#each projects as project}
+    <a href="/projects/{project.file}"
+      ><img
+        src="/thumbnails/{project.file}.png"
+        alt=""
+      />{project.name} by {project.author}</a
+    >
+  {/each}
 </ul>
 
 <style>
@@ -28,5 +29,8 @@
     display: grid;
     grid-template-columns: auto auto auto auto;
     margin-top: 100px;
+  }
+  img {
+    width: 100%;
   }
 </style>
