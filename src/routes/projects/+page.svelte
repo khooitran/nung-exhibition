2d03c3e7b8c487532012d50a4026c9c7125da92c
khooi-pc <khooi.tr@gmail.com>
2024-10-14 11:05:16 +0700

    update projects entries

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,14 +1,59 @@
 <script>
   let projects = [
     {
-      file: 'bach-khoa-khong-toan-thu',
-      name: 'Bách Khoa Không Toàn Thư',
-      author: 'Trần Khánh Linh',
+      file: 'adopt-to-adore',
+      name: 'Adopt to Adore',
+      author: 'Nguyễn Thu Hải',
     },
     {
       file: 'algorithmic-beauty',
       name: 'Algorithmic Beauty',
       author: 'Từ Bảo Ngân',
+    },
+    {
+      file: 'bach-khoa-khong-toan-thu',
+      name: 'Bách Khoa Không Toàn Thư',
+      author: 'Trần Khánh Linh',
+    },
+    {
+      file: 'changes',
+      name: 'Changes - Perspectives on an Evolving City',
+      author: 'Nguyễn Đặng Hùng Sơn',
+    },
+    {
+      file: 'cultural-archives',
+      name: 'Cultural Archive: Ethnicities',
+      author: 'Đinh Song Linh',
+    },
+    {
+      file: 'dieu-phuc',
+      name: 'Diệu Phục',
+      author: 'Nguyễn Lê Thảo Nguyên',
+    },
+    {
+      file: 'expose',
+      name: 'Expose',
+      author: 'Nguyễn Hoàng Long',
+    },
+    {
+      file: 'ngoc-linh',
+      name: 'Ngọc Linh',
+      author: 'Nguyễn Phương Nhung',
+    },
+    {
+      file: 'sinh-hoc-than-thu',
+      name: 'Sinh Học Thần Thú',
+      author: 'Nguyễn Đỗ Quỳnh Anh',
+    },
+    {
+      file: 'timewise',
+      name: 'Timewise',
+      author: 'Nguyễn An Sơn',
+    },
+    {
+      file: 'yeu-dau',
+      name: 'Yêu Dấu',
+      author: 'Nguyễn Đức Duy',
     },
   ];
 </script>
